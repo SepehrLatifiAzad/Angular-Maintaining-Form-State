21a32254906a2bc1c3150b4c2fc31247c364091e
Sepehr <sepehrlatifi.royal@gmail.com>
2023-10-30 15:19:34 +0300

    routeHistory implemented

--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IRouter } from 'src/app/interface/router.interface';
+import { RouterService } from 'src/app/service/router-service/router.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  routes: IRouter[] = [
+    {
+      label: 'Home',
+      path: '/home',
+      icon: 'home',
+      isActive: false,
+    },
+    {
+      label: 'About',
+      path: '/about',
+      icon: 'info',
+      isActive: false,
+    },
+    {
+      label: 'Contact',
+      path: '/contact',
+      icon: 'contacts',
+      isActive: false,
+    },
+    {
+      label: 'Dashboard',
+      path: '/dashboard',
+      icon: 'dashboard',
+      isActive: false,
+    },
+  ];
+  routeHistory$: Observable<IRouter[]> = this.routerService.getHistory();
+  constructor(private routerService: RouterService) {}
 
+  navigateTo(route: IRouter): void {
+    this.routerService.navigateTo(route);
+  }
 }
