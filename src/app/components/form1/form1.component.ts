5b9a89c0d2588a787f4f88e5f5f0a27ce81ca7d1
Sepehr <sepehrlatifi.royal@gmail.com>
2023-11-03 12:19:38 +0300

    register form email and pass fields implemented

--- src/app/components/form1/form1.component.ts
+++ src/app/components/form1/form1.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { RouterService } from 'src/app/service/router-service/router.service';
 
 @Component({
@@ -12,14 +12,22 @@ export class Form1Component {
   form1: FormGroup = new FormGroup({
     firstName: new FormControl(
       this.savedFormData ? (this.savedFormData as any)['firstName'] : '',
-      []
+      [Validators.required, Validators.minLength(3)]
     ),
     lastName: new FormControl(
       this.savedFormData ? (this.savedFormData as any)['lastName'] : '',
-      []
+      [Validators.required, Validators.minLength(3)]
+    ),
+    email: new FormControl(
+      this.savedFormData ? (this.savedFormData as any)['email'] : '',
+      [Validators.required, Validators.email]
+    ),
+    password: new FormControl(
+      this.savedFormData ? (this.savedFormData as any)['password'] : '',
+      [Validators.required, Validators.minLength(8)]
     ),
   });
-
+  hidePassword: boolean = true;
   constructor(private routerService: RouterService) {}
   saveForm() {
     this.routerService.setFormData(this.form1.value);
